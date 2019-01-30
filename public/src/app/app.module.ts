09ecb6d798d07fc800501cfdfa77cf3f435ed720
Paul Bae <paul.bae.j@gmail.com>
2019-01-30 15:30:58 -0800

    first commit

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LocationComponent } from './location/location.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
